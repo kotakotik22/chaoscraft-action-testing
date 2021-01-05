bf2ba526fcb0ef585b6368f7972b4ee797888d6b
kotakotik22 <61428759+kotakotik22@users.noreply.github.com>
2021-01-05 08:36:29 +0100

    add baby zombies to zombie raid event

--- src/main/java/com/kotakotik/chaoscraft/chaos_events/EventZombieRaid.java
+++ src/main/java/com/kotakotik/chaoscraft/chaos_events/EventZombieRaid.java
@@ -2,9 +2,15 @@ package com.kotakotik.chaoscraft.chaos_events;
 
 import com.kotakotik.chaoscraft.ChaosEvent;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.monster.CreeperEntity;
 import net.minecraft.entity.monster.ZombieEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.network.datasync.DataParameter;
 import net.minecraft.server.MinecraftServer;
+import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
+
+import java.lang.reflect.Field;
+import java.util.Random;
 
 public class EventZombieRaid extends ChaosEvent {
     @Override
@@ -19,12 +25,22 @@ public class EventZombieRaid extends ChaosEvent {
 
     public static final int zombiesToSpawn = 15;
 
+    public static Field refl = ObfuscationReflectionHelper.findField(ZombieEntity.class, "field_184737_bv"); // ZombieEntity.IS_CHILD
     @Override
     public void start(MinecraftServer server) {
         for(ServerPlayerEntity player : server.getPlayerList().getPlayers()) {
             for(int i = 0; i < zombiesToSpawn; i++) {
+                boolean spawnBaby = new Random().nextBoolean();
                 ZombieEntity zombieEntity = new ZombieEntity(player.world);
                 zombieEntity.setPosition(player.getPosX(), player.getPosY(), player.getPosZ());
+                if(spawnBaby) {
+                    try {
+                        DataParameter<Boolean> is_child = (DataParameter<Boolean>) refl.get(zombieEntity);
+                        zombieEntity.getDataManager().set(is_child, true);
+                    } catch (IllegalAccessException e) {
+                        e.printStackTrace();
+                    }
+                }
                 player.world.addEntity(zombieEntity);
             }
         }
